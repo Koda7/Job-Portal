7d9bed965774a8f3d3628dcbf27f5e1a858611cb
Ruthvik Kodati <ruthvikkodati@gmail.com>
2023-11-30 15:46:28 -0500

    Added User Schema

---
+++ backend/schemas/schema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  googleId: String,
+  type: String,
+});
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
